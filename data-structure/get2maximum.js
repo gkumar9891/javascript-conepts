5a75a931f4e4606a808ea98a8e339ffd34d70589
gkumar9891 <gauravkumargk9892@gmail.com>
2022-06-03 20:14:55 +0530

    update

---
+++ data-structure/get2maximum.js
@@ -0,0 +1,21 @@
+let a = [7, 2000, 5, 9, 90, 10090,0, 20, 40,];
+// let greater = 0;
+// let greatest = 0;
+
+a.sort( function(a, b){
+console.log(a[a.length - 1], a[a.length - 2]);
+
+
+// for(let i = 0; i < a.length; i++) {
+//     if(a[i] > greatest) {
+//         let temp = greatest;
+//         greatest = a[i];
+//         greater = temp;
+//     }
+
+//     if(a[i] > greater && a[i] < greatest) {
+//         greater = a[i];
+//     }
+// }
+
+// console.log(greater, greatest);
